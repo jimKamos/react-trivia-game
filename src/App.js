0fa3d93a4535c476acf19efb3235983be58bb105
jim Kamos <jimikamos@gmail.com>
2023-05-23 16:05:42 +0300

    add App component logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CategorySelect from './components/CategorySelect';
+import DifficultySelect from './components/DifficultySelect';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedDifficulty, setSelectedDifficulty] = useState('');
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [quizScore, setQuizScore] = useState(0);
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleDifficultySelect = (difficulty) => {
+    setSelectedDifficulty(difficulty);
+  };
+
+  const handleQuizComplete = (score) => {
+    setQuizScore(score);
+    setQuizStarted(false);
+  };
+
+  const handleStartQuiz = () => {
+    setQuizStarted(true);
+  };
+
+  const handlePlayAgain = () => {
+    setSelectedCategory('');
+    setSelectedDifficulty('');
+    setQuizStarted(false);
+    setQuizScore(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Trivia Game</h1>
+      {!quizStarted && !quizScore ? (
+        <>
+          <CategorySelect onSelectCategory={handleCategorySelect} />
+          <DifficultySelect onSelectDifficulty={handleDifficultySelect} />
+          <button type="button" onClick={handleStartQuiz}>
+            Start Quiz
+          </button>
+        </>
+      ) : (
+        <>
+          {quizStarted && (
+            <Quiz
+              category={selectedCategory}
+              difficulty={selectedDifficulty}
+              onQuizComplete={handleQuizComplete}
+            />
+          )}
+          {quizScore > 0 && (
+            <Result
+              score={quizScore}
+              totalQuestions={5} // Set the total number of questions here
+              onPlayAgain={handlePlayAgain}
+            />
+          )}
+        </>
+      )}
     </div>
   );
 }
