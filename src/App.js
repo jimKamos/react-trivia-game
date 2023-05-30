5ca774f80736ca3fa4ffb38b7b0c5ceb3bb4b1b7
jim Kamos <jimikamos@gmail.com>
2023-05-30 20:03:45 +0300

    fix layout and element colors

--- src/App.js
+++ src/App.js
@@ -107,10 +107,8 @@ function App() {
 
   if (quizStarted && questions.length === 0) {
     return (
-      <div className="d-flex justify-content-center">
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
       </div>
     );
   }
--- src/components/Result.js
+++ src/components/Result.js
@@ -4,11 +4,11 @@ const Result = ({ score, totalQuestions, onPlayAgain }) => {
   return (
     <div>
       <h2>Quiz Result</h2>
-      <div className="card">
-        <p>
+      <div className="card p-4 mb-4">
+        <h3 className="text-center">
           Your Score: {score} / {totalQuestions}
-        </p>
-        <button type="button" className="btn btn-primary mt-3" onClick={onPlayAgain}>
+        </h3>
+        <button type="button" className="btn btn-primary mt-3 w-75 mx-auto" onClick={onPlayAgain}>
           Play Again
         </button>
       </div>
